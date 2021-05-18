54e858c11fb9754b1f44c8d59cde4327542f6093
D DraFz <61958249+ddrafz-mbl@users.noreply.github.com>
2021-05-18 22:10:02 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ resource "aws_instance" "jenkinsec2-dev3" {
     key_name = "linux-key"
     security_groups = ["${aws_security_group.ec2_public_security_group3.name}"]
     user_data = <<-EOF
-		#! /bin/bash
-                sudo yum update -y
-		sudo yum install -y httpd.x86_64
-		sudo service httpd start
-		sudo service httpd enable
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+    #! /bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd.x86_64
+    sudo service httpd start
+    sudo service httpd enable
+    echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
     yum install java-1.8.0-openjdk-devel -y
     curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
     sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
@@ -48,8 +48,7 @@ resource "aws_instance" "jenkinsec2-dev3" {
     systemctl start jenkins
     systemctl status jenkins
     systemctl enable jenkins
-	   EOF
-
+    EOF
     tags = {
       Name = "Deploy-Jenkins4",
       LOB = var.lob_dev
