651478de1378940fbfc7af65d90c398d3ead7f84
D DraFz <61958249+ddrafz-mbl@users.noreply.github.com>
2021-05-18 11:58:19 +0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "region" {default = "ap-southeast-1"}
+variable "instance_type" {default = "t2.micro"}
+variable "ami_id" {default = "ami-02f26adf094f51167"}
+variable "lob_dev" {default = "Jenkins-Dev"}
